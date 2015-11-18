294e1d5ca987db13f7b621b1bf7dde19393dd428
QuintilianoB <quintilianoblumenschein@gmail.com>
2015-11-18 15:58:57 -0200

    Corrigido para Python 3

--- sha_512.py
+++ sha_512.py
@@ -1,24 +1,23 @@
 import crypt
 import hashlib
 
-
 def testpass(cryptpass):
     salt = cryptpass.split('$')[2]
     senha_sha_512 = cryptpass.split('$')[3].split(':')[0]
-    print salt + "\n"
-    print senha_sha_512 + "\n"
-    dicfile = open('/home/jhon/Documents/dic.txt', 'r')
+    print (salt + "\n")
+    print (senha_sha_512 + "\n")
+    dicfile = open('dic.txt', 'r')
 
     for word in dicfile.readlines():
         word = word.strip('\n')
         crypto = crypt.crypt(word,"$6$" + salt)
         senha_teste = crypto.split('$')[3]
-        print senha_teste + "\n"
+        print (senha_teste + "\n")
         if senha_teste == senha_sha_512:
-            print "[+]Senha encontrada: "+word+"\n"
+            print ("[+]Senha encontrada: "+word+"\n")
             return
 
-    print "[-]Password not found.\n"
+    print ("[-]Password not found.\n")
     return
 
 def main():
